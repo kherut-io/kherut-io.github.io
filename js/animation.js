24ee211643bb717d8a388296faa356f0c4386620
Adam Pisula <adampisula@pc-adampisula.home>
2018-09-14 17:47:03 +0200

    Loading screen

---
+++ js/animation.js
@@ -0,0 +1,19 @@
+var logoSize;
+var sizeOnScreeen = 0.5;
+var fontSizeFactor = 1.5;
+var marginFactor = 22.375;
+var borderWidthFactor = 26.43;
+
+($(window).width() > $(window).height()) ? logoSize = sizeOnScreeen * $(window).height() : logoSize = sizeOnScreeen * $(window).width();
+
+logoSize *= 0.71;
+logoSize = Math.round(logoSize);
+
+console.log(logoSize);
+
+$(".animation .frame").width(logoSize);
+$(".animation .frame").height(logoSize);
+$(".animation .frame").css("border-width", Math.round(logoSize / borderWidthFactor));
+
+$(".animation .letter").css("font-size", Math.round(logoSize * fontSizeFactor) + "px");
+$(".animation .letter").css("margin-left", "-" + Math.round(logoSize / marginFactor) + "px");
