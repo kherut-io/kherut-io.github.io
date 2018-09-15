ba937f08708431459bb0d6e68b8bd42d2020311e
Adam Pisula <adampisula@pc-adampisula.home>
2018-09-15 07:50:09 +0200

    Waiting for the font to load

--- js/animation.js
+++ js/animation.js
@@ -3,13 +3,13 @@ var sizeOnScreeen = 0.5;
 var fontSizeFactor = 1.5;
 var marginFactor = 22.375;
 var borderWidthFactor = 26.43;
+var fadingTime = 350;
+var fontLoaded = false;
 
 ($(window).width() > $(window).height()) ? logoSize = sizeOnScreeen * $(window).height() : logoSize = sizeOnScreeen * $(window).width();
 
 logoSize *= 0.71;
 logoSize = Math.round(logoSize);
-
-console.log(logoSize);
 
 $(".animation .frame").width(logoSize);
 $(".animation .frame").height(logoSize);
@@ -18,9 +18,15 @@ $(".animation .frame").css("border-width", Math.round(logoSize / borderWidthFact
 $(".animation .letter").css("font-size", Math.round(logoSize * fontSizeFactor) + "px");
 $(".animation .letter").css("margin-left", "-" + Math.round(logoSize / marginFactor) + "px");
 
-$("body").css("overflow", "hidden");
+var interval = setInterval(function() {
+    if(document.fonts.check(Math.round(logoSize * fontSizeFactor) + "px Poppins")) {
+        clearInterval(interval);
 
-setTimeout(function() {
-    $(".animation").fadeOut(350);
-    $("body").css("overflow", "auto");
-}, 3500);
+        setInterval(function() { $(".animation .letter").fadeIn(fadingTime); }, 50);
+
+        setTimeout(function() {
+            $(".animation").fadeOut(fadingTime);
+            setTimeout(function() { $("body").css("overflow", "auto"); }, fadingTime);
+        }, 3500);
+    }
+}, 50);
