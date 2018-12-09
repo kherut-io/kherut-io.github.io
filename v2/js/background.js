2d25633a9a929f9f06eada9b2f85d658ca81d531
Adam Pisula <adam.pisula@outlook.com>
2018-12-09 17:45:11 +0100

    V2 - loading screen, loader

---
+++ v2/js/background.js
@@ -0,0 +1,5 @@
+function setBackground() {
+    var img = new Image();
+    img.load("img/backgrounds/danakil.jpg");
+    //img.load("https://upload.wikimedia.org/wikipedia/commons/3/38/Jan_Matejko%2C_Bitwa_pod_Grunwaldem.jpg");
+}
--- v2/js/load.js
+++ v2/js/load.js
@@ -1,28 +1,42 @@
 Image.prototype.load = function(url){
     var thisImg = this;
     var xmlHTTP = new XMLHttpRequest();
+
     xmlHTTP.open('GET', url,true);
     xmlHTTP.responseType = 'arraybuffer';
+
     xmlHTTP.onload = function(e) {
         var blob = new Blob([this.response]);
         thisImg.src = window.URL.createObjectURL(blob);
+
+        document.getElementsByClassName("background")[0].appendChild(thisImg);
+        var watching = fit(thisImg, document.getElementsByClassName("background")[0], { hAlign: fit.CENTER, vAlign: fit.CENTER, cover: true, apply: true, watch: true });
+
+        setTimeout(function() {
+            window.dispatchEvent(new Event('resize'));
+            document.getElementsByClassName("loading")[0].classList.add("hidden");
+
+            setTimeout(function() { document.getElementsByClassName("loading")[0].style.display = "none"; }, 300);
+        }, 250);
     };
+
     xmlHTTP.onprogress = function(e) {
         thisImg.completedPercentage = parseInt((e.loaded / e.total) * 100);
+        drawLines(thisImg.completedPercentage);
     };
+
     xmlHTTP.onloadstart = function() {
         thisImg.completedPercentage = 0;
     };
+
     xmlHTTP.send();
 };
 
 Image.prototype.completedPercentage = 0;
 
 function drawLines(percentage) {
-    console.log(percentage);
-
     if(percentage <= 16) document.getElementsByClassName("left")[0].style.height = parseInt(percentage / 16 * 205) + "px";
-    else if(percentage <= 50) document.getElementsByClassName("bottom")[0].style.width = parseInt((percentage - 16) / 34 * 428) + "px";
-    else if(percentage <= 66) document.getElementsByClassName("right")[0].style.height = parseInt((percentage - 50) / 16 * 205) + "px";
-    else document.getElementsByClassName("top")[0].style.width = parseInt((percentage - 66) / 34 * 428) + "px";
+    else if(percentage <= 50)  { document.getElementsByClassName("bottom")[0].style.width = parseInt((percentage - 16) / 34 * 428) + "px"; drawLines(16); }
+    else if(percentage <= 66) { document.getElementsByClassName("right")[0].style.height = parseInt((percentage - 50) / 16 * 205) + "px"; drawLines(50); }
+    else { document.getElementsByClassName("top")[0].style.width = parseInt((percentage - 66) / 34 * 428) + "px"; drawLines(66); }
 }
