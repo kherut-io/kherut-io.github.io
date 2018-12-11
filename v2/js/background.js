40e6e02a28f64f873ed2fc0510b1cff3e5885829
Adam Pisula <adam.pisula@outlook.com>
2018-12-11 19:37:43 +0100

    V2 - ?noloading fix

--- v2/js/background.js
+++ v2/js/background.js
@@ -5,6 +5,11 @@ function setBackground() {
         var img = new Image();
         img.load(path);
     } else {
-        document.getElementsByClassName("background")[0].innerHTML += "<img src=\"" + path + "\">";
+        document.getElementsByClassName("background")[0].innerHTML += "<img id=\"bg-img\" src=\"" + path + "\">";
+        var watching = fit(document.getElementById("bg-img"), document.getElementsByClassName("background")[0], { hAlign: fit.CENTER, vAlign: fit.CENTER, cover: true, apply: true, watch: true });
+
+        document.getElementById("bg-img").onload = function() {
+                window.dispatchEvent(new Event('resize'));
+        };
     }
 }
