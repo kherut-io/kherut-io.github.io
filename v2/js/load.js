b3f0df4e6cc9fb4c10309588a9d755ffe15b9dee
Adam Pisula <adam.pisula@outlook.com>
2018-12-11 19:04:19 +0100

    V2 - home screen (mobile portrait)

--- v2/js/load.js
+++ v2/js/load.js
@@ -35,8 +35,8 @@ Image.prototype.load = function(url){
 Image.prototype.completedPercentage = 0;
 
 function drawLines(percentage) {
-    if(percentage <= 16) document.getElementsByClassName("left")[0].style.height = parseInt(percentage / 16 * 205) + "px";
-    else if(percentage <= 50)  { document.getElementsByClassName("bottom")[0].style.width = parseInt((percentage - 16) / 34 * 428) + "px"; drawLines(16); }
-    else if(percentage <= 66) { document.getElementsByClassName("right")[0].style.height = parseInt((percentage - 50) / 16 * 205) + "px"; drawLines(50); }
-    else { document.getElementsByClassName("top")[0].style.width = parseInt((percentage - 66) / 34 * 428) + "px"; drawLines(66); }
+    if(percentage <= 16) document.getElementsByClassName("left")[0].style.height = (parseInt(percentage / 16 * document.getElementsByClassName("outline-logo")[0].offsetHeight) + 3) + "px";
+    else if(percentage <= 50)  { document.getElementsByClassName("bottom")[0].style.width = (parseInt((percentage - 16) / 34 * document.getElementsByClassName("outline-logo")[0].offsetWidth) + 3) + "px"; drawLines(16); }
+    else if(percentage <= 66) { document.getElementsByClassName("right")[0].style.height = (parseInt((percentage - 50) / 16 * document.getElementsByClassName("outline-logo")[0].offsetHeight) + 3) + "px"; drawLines(50); }
+    else { document.getElementsByClassName("top")[0].style.width = (parseInt((percentage - 66) / 34 * document.getElementsByClassName("outline-logo")[0].offsetWidth) + 3) + "px"; drawLines(66); }
 }
